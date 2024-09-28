b6cc48676f9b6467148645df6be98f749c9f09a8
Angel Morales Chazari <artes23graficas@gmail.com>
2024-09-28 14:18:27 -0600

    Add files via upload

---
+++ SegundoEjercicio.java
@@ -0,0 +1,6 @@
+//Escribir un programa que muestre por pantalla el resultado de la siguiente operación aritmética
+//((3.0 + 2) / (2.0 * 5), 2);
+
+public class SegundoEjercicio {
+    
+}
---
+++ SextoEjercicio.java
@@ -0,0 +1,8 @@
+//Escribir un programa que pida al usuario dos números enteros y muestre por pantalla la <n> entre <m> 
+//da un cociente <c> y un resto <r> donde <n> y <m> son los números introducidos por el usuario, y <c> y <r> 
+//son el cociente y el resto de la división entera respectivamente.
+//ejemplo: 27 / 4 = 6.75, pero solo tomaremos el entero y al ser 6 * 4 = 24 (cociente) y faltan 3 (resto) para completar 27
+
+public class SextoEjercicio {
+    
+}
